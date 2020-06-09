c0125ba3bc2620114cff63d579ef2055a4548ef6
stanniboyyy <stanniboyyy@gmail.com>
2020-06-09 13:37:32 +0200

    Updated for 64 bit. * removed boost dependency * updated to c++17 * fixed crash on unload by waiting till the server has properly shut down, as per: https://docs.websocketpp.org/faq.html

--- RCONPlugin/RCONPlugin.cpp
+++ RCONPlugin/RCONPlugin.cpp
@@ -3,46 +3,53 @@
 #include "utils/parser.h"
 #include <iostream>
 
-BAKKESMOD_PLUGIN(RCONPlugin, "RCON plugin", "0.1", PLUGINTYPE_THREADED)
+BAKKESMOD_PLUGIN(RCONPlugin, "RCON plugin", "0.2", PLUGINTYPE_THREADED)
+
 
 bool RCONPlugin::is_authenticated(connection_ptr hdl) {
-	if (auths.find(hdl) == auths.end()) {
+	if (auths.find(hdl) == auths.end())
+	{
 		connection_data conData;
 		conData.authenticated = false;
 
 		auths.insert(std::pair<connection_ptr, connection_data>(hdl, conData));
 	}
+
 	return auths[hdl].authenticated;
 }
 
-string stringify(std::vector<std::string> params)
+std::string stringify(std::vector<std::string> params)
 {
-	string str = "";
+	std::string str = "";
 	for (size_t i = 1; i < params.size(); ++i)
 	{
 		str += params.at(i);
-		if (i != params.size() - 1) {
+		if (i != params.size() - 1)
+		{
 			str += " ";
 		}
 	}
 	str += "";
+
 	return str;
 }
 
 void RCONPlugin::on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
 	connection_ptr con = s->get_con_from_hdl(hdl);
-	try {
+	try
+	{
 		auto input = parseConsoleInput(msg->get_payload());
 		//cvarManager->log(msg->get_payload());
 		if (!is_authenticated(con))
 		{
 			if (input->size() > 0 && input->at(0).size() == 2 && input->at(0).at(0).compare("rcon_password") == 0)
 			{
-				string authKey = cvarManager->getCvar("rcon_password").getStringValue();
-				string userAuthKey = input->at(0).at(1);
+				std::string authKey = cvarManager->getCvar("rcon_password").getStringValue();
+				std::string userAuthKey = input->at(0).at(1);
 				if (!authKey.empty() && !userAuthKey.empty())
 				{
-					if (authKey.compare(userAuthKey) == 0) {
+					if (authKey.compare(userAuthKey) == 0)
+					{
 						auths[con].authenticated = true;
 						s->send(hdl, "authyes", msg->get_opcode());
 						delete input;
@@ -55,24 +62,26 @@ void RCONPlugin::on_message(server* s, websocketpp::connection_hdl hdl, message_
 			delete input;
 			return;
 		}
-		string payload = msg->get_payload();
+		std::string payload = msg->get_payload();
 		gameWrapper->Execute([payload, &_cvarManager = cvarManager, log = *logRcon](GameWrapper* gw) {
-
 			_cvarManager->executeCommand(payload, log);
 		});
 
 		delete input;
 	}
-	catch (const websocketpp::lib::error_code& e) {
+	catch (const websocketpp::lib::error_code& e)
+	{
 		std::cout << "Echo failed because: " << e
 			<< "(" << e.message() << ")" << std::endl;
 		//cvarManager->log("Websocket error");
 	}
 }
 
-
-void RCONPlugin::run_server() {
-	try {
+void RCONPlugin::run_server()
+{
+	try
+	{
+		std::lock_guard<std::mutex> lock(server_running_mutex);
 		// Set logging settings
 		ws_server.set_access_channels(websocketpp::log::alevel::all);
 		//ws_server.set_access_channels(websocketpp::log::alevel::access_core);
@@ -99,10 +108,12 @@ void RCONPlugin::run_server() {
 		// Start the ASIO io_service run loop
 		ws_server.run();
 	}
-	catch (websocketpp::exception const & e) {
+	catch (websocketpp::exception const & e)
+	{
 		std::cout << e.what() << std::endl;
 	}
-	catch (...) {
+	catch (...)
+	{
 		std::cout << "other exception" << std::endl;
 	}
 }
@@ -114,40 +125,40 @@ void RCONPlugin::onLoad()
 	cvarManager->registerCvar("rcon_port", "9002"); //Registered in the main dll now
 	cvarManager->registerCvar("rcon_timeout", "5");
 	cvarManager->registerCvar("rcon_log", "0", "Log all incoming rcon commands", true, true, 0, true, 1, true).bindTo(logRcon);
-	cvarManager->registerNotifier("rcon_disconnect", [this](vector<string> commands) {
-		ws_server.stop_listening();
-		for (auth_iter iterator = auths.begin(); iterator != auths.end(); iterator++)
+
+	cvarManager->registerNotifier("rcon_disconnect", [this](std::vector<std::string> commands) {
+		for (auto& [connection, data] : auths)
 		{
-			if (iterator->first.get()->get_state() == websocketpp::session::state::open)
+			if (connection->get_state() == websocketpp::session::state::open)
 			{
-				iterator->first->close(1000, "Shutting down BM");
+				connection->close(1000, "User requested disconnect");
 				cvarManager->log("Closing websocket connection");
 			}
 		}
-		ws_server.stop();
 	}, "Disconnects all rcon connections", PERMISSION_ALL);
-	cvarManager->registerNotifier("sendback", [this](vector<string> commands) {
-		if (commands.size() > 1)
+
+	cvarManager->registerNotifier("sendback", [this](std::vector<std::string> commands) {
+		if (commands.empty())
+			return;
+
+		for (auto& [connection, data] : auths)
 		{
-			for (auth_iter iterator = auths.begin(); iterator != auths.end(); iterator++)
+			if (is_authenticated(connection) && connection->get_state() == websocketpp::session::state::open)
 			{
-				if (is_authenticated(iterator->first) && iterator->first.get()->get_state() == websocketpp::session::state::open)
-				{
-					iterator->first->send(stringify(commands));
-				}
+				connection->send(stringify(commands));
 			}
 		}
 	}, "Sends text to all connected clients. Usage: sendback abc def ghi \"hij klm\"", PERMISSION_ALL); 
 
-	cvarManager->registerNotifier("ws_inventory", [this](vector<string> commands) {
+	cvarManager->registerNotifier("ws_inventory", [this](std::vector<std::string> commands) {
 		if (commands.size() < 3)
 			return;
-		cvarManager->log("Sending invent");
-		for (auth_iter iterator = auths.begin(); iterator != auths.end(); iterator++)
-		{
-			if (is_authenticated(iterator->first) && iterator->first.get()->get_state() == websocketpp::session::state::open)
-			{
 
+		cvarManager->log("Sending inventory");
+		for (auto& [connection, data] : auths)
+		{
+			if (is_authenticated(connection) && connection->get_state() == websocketpp::session::state::open)
+			{
 				std::string str = "invent_dump";
 				for (int i = 1; i < 3; i++)
 				{
@@ -159,32 +170,32 @@ void RCONPlugin::onLoad()
 				std::string content((std::istreambuf_iterator<char>(ifs)),
 					(std::istreambuf_iterator<char>()));
 
-
-				string::size_type pos = 0; // Must initialize
+				std::string::size_type pos = 0; // Must initialize
 				std::stringstream a;
 				a << std::endl;
 				std::string endl = a.str();
-				while ((pos = content.find(endl, pos)) != string::npos)
+				while ((pos = content.find(endl, pos)) != std::string::npos)
 				{
 					content.erase(pos, endl.size());
 				}
-				auto ec = iterator->first->send(content + "\n", websocketpp::frame::opcode::binary);
+				auto ec = connection->send(content + "\n", websocketpp::frame::opcode::binary);
 
 				cvarManager->log(ec.message());
 			}
 		}
 
-	}, "Sends text to all connected clients. Usage: sendback abc def ghi \"hij klm\"", PERMISSION_ALL);
-	//thread t(run_server);
+	}, "Sends inventory dump to all connected clients. Usage: ws_inventory [all] (csv|json)", PERMISSION_ALL);
+
 	run_server();
-	//ws_server.run();
 }
 
 void RCONPlugin::onUnload()
 {
-	if (!ws_server.stopped()) {
+	if (ws_server.is_listening())
+	{
 		ws_server.stop_listening();
-		for (auth_iter iterator = auths.begin(); iterator != auths.end(); )
+		auth_iter iterator = auths.begin();
+		while (iterator != auths.end())
 		{
 			if (iterator->first.get()->get_state() == websocketpp::session::state::open)
 			{
@@ -193,6 +204,7 @@ void RCONPlugin::onUnload()
 			}
 			iterator = auths.erase(iterator);
 		}
-		ws_server.stop();
+		// Wait for the server to close cleanly.
+		server_running_mutex.lock();
 	}
 }
